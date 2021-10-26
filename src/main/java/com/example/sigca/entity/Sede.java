da4fb48fe059df5c3d147a8210e198afb47a1a87
josueprogram2003 <73667027+josueprogram2003@users.noreply.github.com>
2021-10-26 13:19:38 -0500

    Cambios josue en el tbl disitrito

--- src/main/java/com/example/sigca/entity/Sede.java
+++ src/main/java/com/example/sigca/entity/Sede.java
@@ -1,11 +1,19 @@
 package com.example.sigca.entity;
 
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,14 +23,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name="tbl_sede")
-public class Sede {
-	//git log --oneline --decorate --all --graph
+public class Sede implements Serializable{
+	
+	private static final long serialVersionUID = -1942395296462336245L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id_sede")
 	public int idsede;
 	@Column(name="no_sede")
 	public String nombreSede;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name="id_distrito")
+	private Set<Distrito> distrito;
 	
 	
 }
