4ac90054de50c791474920ebb6331b712898507a
Alvaro <alvaroalva@upeu.edu.pe>
2021-10-26 11:16:38 -0500

    de josue

---
+++ src/main/java/com/example/sigca/SigcaJosue.java
@@ -0,0 +1,17 @@
+package com.example.sigca;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+
+public class SigcaJosue implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigcaJosue.class, args);
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
