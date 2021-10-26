f6ee5a6aaa6a5eec31ab7a1c25c1d4181e0403e0
ElvisRequejo <elvizrequejopaz@gmail.com>
2021-10-26 17:50:35 -0500

    Actualizar Datos TIpo

--- src/main/java/com/example/sigca/SigcaApplication.java
+++ src/main/java/com/example/sigca/SigcaApplication.java
@@ -13,11 +13,13 @@ import com.example.sigca.entity.Distrito;
 import com.example.sigca.entity.Modulo;
 import com.example.sigca.entity.Persona;
 import com.example.sigca.entity.Sede;
+import com.example.sigca.entity.Tipo;
 import com.example.sigca.repository.CategoriaRepository;
 import com.example.sigca.repository.DistritoRepository;
 import com.example.sigca.repository.ModuloRepository;
 import com.example.sigca.repository.PersonaRepository;
 import com.example.sigca.repository.SedeRepository;
+import com.example.sigca.repository.TipoRepository;
 
 @SpringBootApplication
 public class SigcaApplication implements CommandLineRunner{
@@ -35,6 +37,9 @@ private CategoriaRepository cate;
 
 @Autowired 
 private ModuloRepository modulo;
+
+@Autowired 
+private TipoRepository tipo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SigcaApplication.class, args);
@@ -55,21 +60,6 @@ private ModuloRepository modulo;
 		for(int i=0;i<pe.size(); i++) {
 			System.out.println(pe.get(i).getNombre()+" "+pe.get(i).getPaterno());
 		}*/
-		
-		
-		
-		Modulo m = modulo.buscarModulo(1);
-		System.out.println(m.getId_modulo()+ " : " + m.getNo_modulo()+ "  "+ m.getCategoria().getNo_categoria());
-		//cate.actualizarCategoria(4, "Deportivo");
-		List<Modulo> l = modulo.listarModulo();
-		for(int i=0;i<l.size(); i++) {
-			System.out.println(l.get(i).getId_modulo()+" "+l.get(i).getNo_modulo()+ " " +l.get(i).getCategoria().getNo_categoria());
-		}
-		
-		List<Categoria> c = cate.listarCategoria();
-		for(int i=0;i<c.size(); i++) {
-			System.out.println(c.get(i).getId_categoria()+" "+c.get(i).getNo_categoria());
-		}
 		
 }
 }
--- src/main/java/com/example/sigca/entity/Tipo.java
+++ src/main/java/com/example/sigca/entity/Tipo.java
@@ -1,5 +1,35 @@
 package com.example.sigca.entity;
 
-public class Tipo {
+import java.io.Serializable;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="tbl_tipo")
+public class Tipo implements Serializable {
+		private static final long serialVersionUID = -1942395296462336245L;
+		@Id
+		@GeneratedValue (strategy = GenerationType.IDENTITY)
+		@Column(name = "id_tipo")
+		private int id;
+		@Column(name="no_tipo")
+		private String nombre;
+		/*@OneToMany(cascade = CascadeType.ALL)
+		@JoinColumn(name="id_recurso")
+		private Set<Recurso> recursos;*/
 }
