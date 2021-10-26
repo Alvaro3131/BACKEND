b79de664622491e3aa8c8aef55b0a40da959a6bc
Alvaro <alvaroalva@upeu.edu.pe>
2021-10-25 21:44:08 -0500

    Esta es otra tabla

---
+++ src/main/java/com/example/sigca/entity/Distrito.java
@@ -0,0 +1,7 @@
+package com.example.sigca.entity;
+
+public class Distrito {
+public static void main(String[] args) {
+	System.out.println("Hola");
+}
+}
--- src/main/java/com/example/sigca/repository/PersonaRepository.java
+++ src/main/java/com/example/sigca/repository/PersonaRepository.java
@@ -19,6 +19,12 @@ public interface PersonaRepository extends JpaRepository<Persona, Integer>{
 	@Query(value = "{CALL SP_LISTAR_TBL_PERSONA(:V_ID)}", nativeQuery = true) 
 	Optional<Persona> readId(@Param("V_ID") int V_ID);
 	
+	@Query(value = "{ CALL SP_DEL_TBL_PERSONA(:P_nID) }", nativeQuery = true) 
+	String deleteId(@Param("P_nID") int P_nID);
+	
+
+	
+	
 	@Query(value = "{ CALL ido_bi_capa.SP_INS_TBL_PERSONA(:p_vNOMBRE,:p_vAPELLIDO_P,:p_vAPELLIDO_M,:p_vDNI,:p_vCORREO,:p_vTELEFONO) }", nativeQuery = true) 
 	String savePersona(@Param("p_vNOMBRE") String p_vNOMBRE,
 			@Param("p_vAPELLIDO_P") String p_vAPELLIDO_P, @Param("p_vAPELLIDO_M") String p_vAPELLIDO_M,
