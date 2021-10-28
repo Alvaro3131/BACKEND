842492e9d84f26175767864f31cbf8b1bc130280
Ariana Nayeli Villalva <arianavillava@upeu.edu.pe>
2021-10-27 23:49:31 -0500

    tablas Programa Seminario y Seminario

---
+++ src/main/java/com/example/sigca/entity/ProgramaSeminario.java
@@ -0,0 +1,35 @@
+package com.example.sigca.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tbl_programa_seminario" )
+public class ProgramaSeminario {
+ 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_programa_capacitacion")
+	public int id_programa_capacitacion;
+	
+	@ManyToOne
+	@JoinColumn(name="fk_seminario", nullable = false)
+	public Seminario seminario;
+	
+	@ManyToOne
+	@JoinColumn(name="fk_bancocomunal", nullable = false)
+	public BancoComunal banco;
+}
