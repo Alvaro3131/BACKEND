2ad2e30781dc541ce8cf4abe6104ac585801ffe5
Paulo David <66272982+QBay2212@users.noreply.github.com>
2021-10-28 22:21:26 -0500

    OneToMany Sesion-Recurso

--- src/main/java/com/example/sigca/entity/Sesion.java
+++ src/main/java/com/example/sigca/entity/Sesion.java
@@ -1,6 +1,9 @@
 package com.example.sigca.entity;
 
 import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -31,4 +35,9 @@ public class Sesion implements Serializable {
 	@ManyToOne
 	@JoinColumn(name="FK_MODULO",nullable = false)
 	public Modulo modulo;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "id_recurso")
+	private Set<Recurso> recurso;
+	
 }
