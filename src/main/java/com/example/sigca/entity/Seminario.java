799d3a4990fd66c877cd452f0491a6a07b2d0aca
Ariana Nayeli Villalva <arianavillava@upeu.edu.pe>
2021-10-28 22:46:26 -0500

    Cambios en bancocomunal, seminario y programseminario

--- src/main/java/com/example/sigca/entity/Seminario.java
+++ src/main/java/com/example/sigca/entity/Seminario.java
@@ -1,11 +1,16 @@
 package com.example.sigca.entity;
 
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -35,5 +40,14 @@ public class Seminario {
 	
 	@Column(name = "URL_SEMINARIO")
 	public String URL_SEMINARIO;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name="id_asistencia_seminario")
+	private Set<Asistencia_Seminario> as;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name="id_programa_capacitacion")
+	private Set<ProgramaSeminario> ps;
+	
 	
 }
