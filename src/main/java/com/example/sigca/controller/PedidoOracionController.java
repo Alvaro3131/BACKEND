9f3022e677e739dea4f3d4238fd4b2ccc2fb7115
Paulo David <66272982+QBay2212@users.noreply.github.com>
2021-11-12 12:57:45 -0500

    me faltó un ; XD

--- src/main/java/com/example/sigca/controller/PedidoOracionController.java
+++ src/main/java/com/example/sigca/controller/PedidoOracionController.java
@@ -12,9 +12,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RestController;
 import com.example.sigca.entity.PedidoOracion;
-import com.example.sigca.entity.Recurso;
 import com.example.sigca.serviceImpl.PedidoOracionServiceImpl;
 
 @RestController
