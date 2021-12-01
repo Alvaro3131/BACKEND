bebb2d39fa17ac46d838849036fe281f9d94456d
Alvaro <alvaroalva@upeu.edu.pe>
2021-12-01 17:30:04 -0500

    nueva funcion

--- src/main/java/com/example/sigca/service/SeminarioService.java
+++ src/main/java/com/example/sigca/service/SeminarioService.java
@@ -6,6 +6,7 @@ import com.example.sigca.entity.Seminario;
 
 public interface SeminarioService {
 	List<Seminario> listarSeminario();
+	List<Seminario> listarSeminarioSocio(int id);
 	String insertarSeminario(Seminario se);
 	Seminario buscarSeminario(int id_seminario);
 	String eliminarSeminario(int id_seminario);
