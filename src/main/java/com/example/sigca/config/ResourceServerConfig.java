9b22569e73effdb477b117a851ad4ff49dc57bd7
Alvaro <alvaroalva@upeu.edu.pe>
2021-12-12 11:25:55 -0500

    make it better

--- src/main/java/com/example/sigca/config/ResourceServerConfig.java
+++ src/main/java/com/example/sigca/config/ResourceServerConfig.java
@@ -30,7 +30,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(Arrays.asList("https://sigca-54467.web.app"));
+		config.setAllowedOrigins(Arrays.asList("http://localhost:55282"));
 		config.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
 		config.setAllowCredentials(true);
 		config.setAllowedHeaders(Arrays.asList("Content-Type","Authorization"));
